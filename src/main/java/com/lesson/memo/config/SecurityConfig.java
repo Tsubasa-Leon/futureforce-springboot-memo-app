deae464ee5cde94f6a034d3daa5cfb53fe793206
“TsubasaIwaki” <t.iwaki.cingroup2012@gmail.com>
2026-09-06 21:31:00 +0900

    Spring Securityによる認証機能の実装(修正版2)

--- src/main/java/com/lesson/memo/config/SecurityConfig.java
+++ src/main/java/com/lesson/memo/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	        http
 	            .authorizeHttpRequests(authz -> authz
-	                .requestMatchers("/admin/signin", "/admin/signup","/css/**","/js/**").permitAll()
+	                .requestMatchers("/admin/signin", "/admin/signup","/admin/create","/css/**","/js/**").permitAll()
 	                .anyRequest().authenticated()
 	            )
 	            
